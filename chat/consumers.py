1297aec885fb7d3b8f9c65647ad987f71f7953f9
Ennyola <medunoyeeni@gmail.com>
2022-01-19 17:55:58 +0100

    Added redis configuration in channel_layers

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,20 +1,41 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         print("Connection Established")
-        self.accept()
+        self.user_name = self.scope['url_route']['kwargs']['username']
+        self.room_group_name = f'chat_{self.user_name}'
 
-    def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
 
-    def receive(self, text_data):
-        print(text_data)
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        text_data = json.dumps({'message': message})
-        self.send(
-            text_data
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
         )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        print(event)
+        message = event['message']
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
