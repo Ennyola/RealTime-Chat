a5970ff2ab6e3bfeb63bbcf14b362e5dcdd8427d
Ennyola <medunoyeeni@gmail.com>
2022-01-20 22:00:53 +0100

    models modified

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,13 @@
+from tkinter.font import ROMAN
 from django.contrib import admin
-
+from .models import Room, Message, Participants
 # Register your models here.
+
+
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Participants)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_view = ['user', 'message', 'time', 'status']
--- chat/models.py
+++ chat/models.py
@@ -1,19 +1,22 @@
-from django.core.checks import messages
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Room(models.Model):
-    name=models.CharField(max_length=500)
-    _type=models.BooleanField()
-    
+    name = models.CharField(max_length=500)
+    _type = models.BooleanField()
+
 
 class Participants(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room =  models.ForeignKey(Room, on_delete=models.CASCADE)
-    
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room =  models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     message = models.TextField()
+    time = models.DateTimeField(auto_now=True)
+    type = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
