d519e371ad66ae7bcc39a743d70401432f9cea5e
Ennyola <medunoyeeni@gmail.com>
2022-01-24 15:33:23 +0100

    display message status

--- chat/static/chat/js/index.js
+++ chat/static/chat/js/index.js
@@ -1,6 +1,8 @@
 let inputBox = document.querySelector("#input-message")
 let sendButton = document.querySelector("#send-button")
 let chatHolder = document.querySelectorAll(".messages")[0]
+let icon = document.querySelector('.chat-status i:nth-child(1)')
+
 
 inputBox.focus()
 inputBox.addEventListener('keyup', (e => {
@@ -18,17 +20,22 @@ chatSocket.addEventListener('open', (e) => {
     console.log("Connection Established")
 })
 chatSocket.addEventListener('message', (e) => {
-    const divOutgoing = document.createElement("div")
-    const divchatBubble = document.createElement("div")
-    const divMsg = document.createElement("div")
     const message = JSON.parse(e.data)
-    divOutgoing.className = "outgoing-message"
-    divchatBubble.className = "chat-bubble"
-    divMsg.className = "msg"
-    divMsg.innerHTML = message.message
-    divchatBubble.appendChild(divMsg)
-    divOutgoing.append(divchatBubble)
-    chatHolder.append(divOutgoing)
+    let str = `<div class="outgoing-message">
+                    <div class="chat-bubble">
+                        <div class="msg">${message.message}</div>
+                        <span class ="msg-metadata">
+                            <span class = "msg-time"></span> 
+                            <span class="chat-status">
+                                <i class="fas fa-check"></i>
+                            </span>
+                        </span>
+                    </div>
+                </div>`
+    console.log(icon.classList)
+
+
+
 })
 chatSocket.addEventListener('close', (e) => {
     console.error('Chat socket closed unexpectedly');
@@ -38,5 +45,31 @@ sendButton.addEventListener('click', (e) => {
     chatSocket.send(JSON.stringify({
         'message': inputBox.value
     }));
+    let str = `<div class="outgoing-message">
+                    <div class="chat-bubble">
+                        <div class="msg">${inputBox.value}</div>
+                        <span class ="msg-metadata">
+                            <span class = "msg-time"></span> 
+                            <span class="chat-status">
+                                <i class="fas fa-spinner"></i>
+                            </span>
+                        </span>
+                    </div>
+                </div>`
+
+    chatHolder.innerHTML += str
+    chatHolder.scrollTop = chatHolder.scrollHeight;
     inputBox.value = ""
+
 })
+
+// const divOutgoing = document.createElement("div")
+//     const divchatBubble = document.createElement("div")
+//     const divMsg = document.createElement("div")
+
+//     divOutgoing.className = "outgoing-message"
+//     divchatBubble.className = "chat-bubble"
+//     divMsg.className = "msg"
+//     divMsg.innerHTML = inputBox.value
+//     divchatBubble.appendChild(divMsg)
+//     divOutgoing.append(divchatBubble)
