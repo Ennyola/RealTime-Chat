fb2f023f92e82274ccfc71b14e880a1756db471c
Ennyola <medunoyeeni@gmail.com>
2022-12-01 20:36:21 +0100

    added a context mixin to chat.views

--- chat/models.py
+++ chat/models.py
@@ -13,9 +13,10 @@ class Room(models.Model):
     def __str__(self) -> str:
         return f"{self.name}"
 
+
 class Participants(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name="participant", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.room.name} {self.user}"
@@ -30,12 +31,15 @@ class Participants(models.Model):
             friend_list.append({"room_id": room_id, "room_name": room_name})
         return friend_list
 
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name="message", on_delete=models.CASCADE)
     content = models.TextField()
     time = models.DateTimeField(default=timezone.localtime)
-    message_type = models.CharField(db_column="type", max_length=50, blank=True, null=True)
+    message_type = models.CharField(
+        db_column="type", max_length=50, blank=True, null=True
+    )
     status = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self) -> str:
--- chat/views.py
+++ chat/views.py
@@ -9,38 +9,33 @@ from .helpers import get_room_name
 
 # Create your views here.
 
-class ChatIndexView(View):
-    
-    def get(self,request,*args,**kwargs):
+
+class FriendListMixin:
+    def get_context_data(self, **kwargs):
+        user = kwargs.get("user")
+        context = {}
+        context["friends"] = Participants.get_friends(user)
+        return context
+
+
+class ChatIndexView(FriendListMixin, View):
+    def get(self, request, *args, **kwargs):
+
         friend_list = Participants.get_friends(request.user)
-        context = {
-            "friends": friend_list,
-        }
+        context = super().get_context_data(user=request.user)
         return render(request, "chat/index.html", context)
 
 
-class ChatRoomView(View):
-    def get(self,request,**kwargs):
-        messages = Message.objects.filter(room_id=kwargs['room_id'])
-        friend_list = Participants.get_friends(request.user)
-        room = Room.objects.get(id=kwargs['room_id'])
-        context = {
-            "messages": messages,
-            "room_name": get_room_name(room.name, request.user.username),
-            "friends": friend_list,
-        }
+class ChatRoomView(FriendListMixin, View):
+    def get(self, request, **kwargs):
+        messages = Message.objects.filter(room_id=kwargs["room_id"])
+        room = Room.objects.get(id=kwargs["room_id"])
+        context = super().get_context_data(user=request.user)
+        context.update(
+            {
+                "messages": messages,
+                "room_name": get_room_name(room.name, request.user.username),
+            }
+        )
         return render(request, "chat/chat-room.html", context)
-        
 
-# @login_required
-# def chat_room(request, room_id):
-#     messages = Message.objects.filter(room_id=room_id)
-#     friend_list = Participants.get_friends(request.user)
-#     room = Room.objects.get(id=room_id)
-
-#     context = {
-#         "messages": messages,
-#         "room_name": get_room_name(room.name, request.user.username),
-#         "friends": friend_list,
-#     }
-#     return render(request, "chat/chat-room.html", context)
