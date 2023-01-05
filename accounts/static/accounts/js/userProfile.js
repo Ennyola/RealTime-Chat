757d8cb1f7a1b537e2c4206e978c9336ed7dacad
Ennyola <medunoyeeni@gmail.com>
2023-01-05 13:48:19 +0100

    added form to the popover

--- accounts/static/accounts/js/userProfile.js
+++ accounts/static/accounts/js/userProfile.js
@@ -1,10 +1,20 @@
-let a = document.querySelector('.popover-option')
-let popover = new bootstrap.Popover(a, {
-    html: true,
-    content: () => {
-        return document.querySelector('.popover-list').innerHTML
-    },
-    trigger: 'focus',
-    placement: 'right'
+let myDefaultAllowList = bootstrap.Tooltip.Default.allowList
 
+// Enabling our popover to be used with bootstrap
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl, {
+        html: true,
+        content: () => {
+            return document.querySelector('.popover-list').innerHTML
+        },
+        container: 'body',
+        trigger: 'focus',
+        placement: 'right',
+        // sanitize: false,
+    })
 })
+
+// To allow the use of form and inputs in the popover
+myDefaultAllowList.form = ['action', 'method']
+myDefaultAllowList.input = ['type', 'name', 'value', 'placeholder', 'autocomplete', 'required', 'id']
