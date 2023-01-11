1f9270203328eed5e324f8132f1c71671fff3710
Ennyola <medunoyeeni@gmail.com>
2023-01-11 10:48:37 +0100

    take photo functionality now added

--- accounts/static/accounts/js/userProfile.js
+++ accounts/static/accounts/js/userProfile.js
@@ -1,4 +1,12 @@
+let takePhotoBtn = document.querySelector("#take-photo");
+let changeDpVideo = document.querySelector("#video");
+let capture = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
 let myDefaultAllowList = bootstrap.Tooltip.Default.allowList
+
+console.log(takePhotoBtn)
+console.log(changeDpVideo)
+console.log(";how")
 
 // Enabling our popover to be used with bootstrap
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
@@ -18,3 +26,17 @@ let popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
 myDefaultAllowList.form = ['action', 'method', 'enctype']
 myDefaultAllowList.input = ['type', 'name', 'value', 'placeholder', 'autocomplete', 'required', 'id']
 myDefaultAllowList.button = ['type', 'name', 'value', 'id']
+
+takePhotoBtn.addEventListener("click", () => {
+    console.log("here")
+    let stream = navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    changeDpVideo.srcObject = stream;
+})
+
+capture.addEventListener("click", () => {
+    canvas.getContext('2d').drawImage(changeDpVideo, 0, 0, canvas.width, canvas.height);
+    let image_data_url = canvas.toDataURL('image/jpeg');
+
+    // data url of the image
+    console.log(image_data_url);
+})
