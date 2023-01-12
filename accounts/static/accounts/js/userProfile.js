dd0b5ac61d506cf3e68cea109754a4db3daa55ed
Ennyola <medunoyeeni@gmail.com>
2023-01-12 17:27:14 +0100

    Can now trigger take-photo button event in popover

--- accounts/static/accounts/js/userProfile.js
+++ accounts/static/accounts/js/userProfile.js
@@ -1,37 +1,39 @@
-let takePhotoBtn = document.querySelector("#take-photo");
 let changeDpVideo = document.querySelector("#video");
 let capture = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
+let cameraIcon = document.querySelector("#camera-icon");
 let myDefaultAllowList = bootstrap.Tooltip.Default.allowList
 
-console.log(takePhotoBtn)
-console.log(changeDpVideo)
-console.log(";how")
-
 // Enabling our popover to be used with bootstrap
-let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-let popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl, {
-        html: true,
-        content: () => {
-            return document.querySelector('.popover-list').innerHTML
-        },
-        container: 'body',
-        // trigger: 'focus',
-        placement: 'right',
-    })
+let popover = new bootstrap.Popover(cameraIcon, {
+    html: true,
+    content: () => {
+        return document.querySelector('.popover-list').innerHTML
+    },
+    container: 'body',
+    // trigger: 'focus',
+    placement: 'right',
+    delay: { "show": 500, "hide": 100 }
 })
+
+// Triggering the take-photo button event within the popover when the popover is appears in the DOM
+cameraIcon.addEventListener('shown.bs.popover', () => {
+    let takePhotoBtn = document.querySelector(".popover-body #take-photo");
+    takePhotoBtn.addEventListener("click", async() => {
+        let stream = navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+        changeDpVideo.srcObject = stream;
+    })
+
+})
+
+
 
 // To allow the use of form, inputs and button elements in the popover
 myDefaultAllowList.form = ['action', 'method', 'enctype']
 myDefaultAllowList.input = ['type', 'name', 'value', 'placeholder', 'autocomplete', 'required', 'id']
 myDefaultAllowList.button = ['type', 'name', 'value', 'id']
 
-takePhotoBtn.addEventListener("click", () => {
-    console.log("here")
-    let stream = navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-    changeDpVideo.srcObject = stream;
-})
+
 
 capture.addEventListener("click", () => {
     canvas.getContext('2d').drawImage(changeDpVideo, 0, 0, canvas.width, canvas.height);
