938c6fc05568841cd05751f68551c7f24d78cee8
Ennyola <medunoyeeni@gmail.com>
2023-01-17 13:58:00 +0100

    pictures taken can now be saved as display picture

--- accounts/static/accounts/js/userProfile.js
+++ accounts/static/accounts/js/userProfile.js
@@ -3,11 +3,12 @@ let capture = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
 let cameraIcon = document.querySelector("#camera-icon");
 let myDefaultAllowList = bootstrap.Tooltip.Default.allowList
-
+const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 // To allow the use of form, inputs and button elements in the popover
 myDefaultAllowList.form = ['action', 'method', 'enctype']
 myDefaultAllowList.input = ['type', 'name', 'value', 'placeholder', 'autocomplete', 'required', 'id']
 myDefaultAllowList.button = ['type', 'name', 'value', 'id']
+
 
 // Enabling our popover to be used with bootstrap
 let popover = new bootstrap.Popover(cameraIcon, {
@@ -21,6 +22,20 @@ let popover = new bootstrap.Popover(cameraIcon, {
     delay: { "show": 500, "hide": 100 }
 })
 
+function dataURLToBlob(dataURL) {
+    var parts = dataURL.split(',');
+    var contentType = parts[0].split(':')[1].split(';')[0];
+    var raw = atob(parts[1]);
+    var rawLength = raw.length;
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], { type: contentType });
+}
+
 // Triggering the take-photo button event within the popover when the popover is appears in the DOM
 cameraIcon.addEventListener('shown.bs.popover', () => {
     let takePhotoBtn = document.querySelector(".popover-body #take-photo");
@@ -33,9 +48,29 @@ cameraIcon.addEventListener('shown.bs.popover', () => {
     })
     capture.addEventListener("click", () => {
         canvas.getContext('2d').drawImage(changeDpVideo, 0, 0, canvas.width, canvas.height);
-        let image_data_url = canvas.toDataURL('image/jpeg')
-        imageInput.value = image_data_url;
-        submitForm.submit()
+        let imageDataUrl = canvas.toDataURL('image/jpeg')
+        const formData = new FormData();
+        // Create a blob object from the data URL
+        let blob = dataURLToBlob(imageDataUrl);
+
+        // Create a file object from the blob
+        let file = new File([blob], "myImage.jpg", { type: "image/jpeg" });
+        console.log(file)
+        formData.append("display_picture", file);
+        fetch("", {
+            method: "POST",
+            body: formData,
+            credentials: 'same-origin',
+            headers: {
+                'X-CSRFToken': csrftoken,
+            }
+        })
+
+        // imageInput.value = image_data_url;
+        // const img = new Image();
+        // img.src = image_data_url;
+        // imageInput.appendChild(img);
+        // submitForm.submit()
 
     })
 
--- accounts/views.py
+++ accounts/views.py
@@ -32,18 +32,19 @@ def user_profile(request, username):
     # Uploads and saves a users display picture.
     print(request.POST, request.FILES)
     if request.method == "POST" and request.FILES:
-        print("hellp")
         form = UpdateProfileForm(request.POST, request.FILES)
         if form.is_valid():
             profile.display_picture = form.cleaned_data["display_picture"]
             profile.save()
             return redirect("user_profile", username=user.username)
+        
     # When the user submits the form, the form is validated and the bio is updated.
     elif request.method == "POST" and "bio" in request.POST:
         form = UpdateProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             return redirect("user_profile", username=user.username)
+        
     # When the user submits the form, the form is validated and the display picture is deleted.
     elif request.method == "POST" and "delete_photo" in request.POST:
         form = UpdateProfileForm(request.POST)
