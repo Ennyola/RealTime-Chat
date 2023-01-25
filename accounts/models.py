a7b85ca349b9fdb8a10272581063896c46252eff
Ennyola <medunoyeeni@gmail.com>
2023-01-25 04:51:10 +0100

    changing the root path of the media display picture

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 from django.conf import settings
 
@@ -5,16 +7,30 @@ from django.conf import settings
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True, related_name="userprofile")
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="userprofile",
+    )
     bio = models.TextField(blank=True)
-    display_picture = models.ImageField(upload_to="profile_pictures", default="profile_pictures/user-icon.png", blank=True,)
+    display_picture = models.ImageField(
+        upload_to="profile_pictures",
+        default="profile_pictures/user-icon.png",
+        blank=True,
+    )
 
     def __str__(self):
         return f"{self.bio} by {self.user.username}"
-    
+
     @property
     def get_image(self):
+        """
+        Returns the display picture of the user or the default image
+        if the user has not uploaded a display picture.
+        """
+        print([settings.BASE_DIR / "media" / "profile_pictures" / "user-icon.png"])
         if not self.display_picture:
-            return "profile_pictures/user-icon.png"
+            return "../media/profile_pictures/user-icon.png"
         else:
             return self.display_picture.url
--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-from django.contrib.auth import get_user_model 
+from django.contrib.auth import get_user_model
 
 from .helpers import get_room_name
 
 # Create your models here.
 
 USER = get_user_model()
+
 
 class Room(models.Model):
     name = models.CharField(max_length=500)
@@ -29,17 +30,17 @@ class Participants(models.Model):
     def get_friends(cls, user) -> list:
         """To get all the friends associated with a user"""
 
-        # This fetchecs all participants in a room having a relationship 
+        # This fetches all participants in a room having a relationship
         # with the current user nd orders the query by the room's last message.
         participants = (
             cls.objects.select_related("room")
             .filter(user=user)
             .order_by("-room__messages__time")
         )
-        
-        # Ordering the rooms using their 
+
+        # Ordering the rooms using their
         # last message returns a queryset with duplicate rooms
-        # The code below removes duplicates rooms 
+        # The code below removes duplicates rooms
         # while still ordering the rooms by their last message
         unique_participants = []
         [
@@ -53,10 +54,7 @@ class Participants(models.Model):
             room_name = get_room_name(friend.room.name, user.username)
             latest_message = friend.room.messages.last()
             friend_user_object = USER.objects.get(username=room_name)
-            if friend_user_object.userprofile.display_picture:
-                display_picture = friend_user_object.userprofile.display_picture.url
-            else:
-                display_picture=""
+            display_picture = friend_user_object.userprofile.get_image
             friend_list.append(
                 {
                     "room_id": room_id,
