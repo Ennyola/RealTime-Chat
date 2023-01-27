dae6cbde17d9ee604128d56073df932ddbfa127e
Ennyola <medunoyeeni@gmail.com>
2023-01-27 14:33:04 +0100

    done with the settings popover options

--- static/js/headerSettings.js
+++ static/js/headerSettings.js
@@ -1,5 +1,9 @@
 let settingsPopover = new bootstrap.Popover(document.querySelector('#settings-icon'), {
     html: true,
     container: 'body',
-
+    content: () => {
+        return document.querySelector('.settings-popover-list').innerHTML
+    },
+    placement: 'right',
+    delay: { "show": 500, "hide": 100 }
 })
