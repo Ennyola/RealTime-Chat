4d788018fb7c1884318006c75c4a5edb38ecc55c
Ennyola <medunoyeeni@gmail.com>
2023-01-30 12:10:54 +0100

    changing the base js structure

--- static/js/base.js
+++ static/js/base.js
@@ -1,1 +1,10 @@
-console.log("base")
+import {
+    invite,
+    handleVideoOfferMsg,
+    handleVideoAnswerMsg,
+    handleNewICECandidateMsg,
+    handleHangUpMsg,
+    hangUpCall
+} from "/static/chat/js/videoCall.js";
+
+console.log(invite)
