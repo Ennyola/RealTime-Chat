90f5e2dcd1ef43f3f18bc47a724e799c75c2c7c9
Ennyola <medunoyeeni@gmail.com>
2023-02-15 13:49:50 +0100

    changes

--- find_friends/urls.py
+++ find_friends/urls.py
@@ -4,6 +4,6 @@ from .views import index, send_or_cancel_request, accept_or_delete_request
 app_name = "find_friends"
 urlpatterns = [
     path("", index, name="index"),
-    path("<int:id>/add/", send_or_cancel_request, name="send_or_cancel_request"),
-    path("<int:id>/accept/", accept_or_delete_request, name="accept_or_delete_request"),
+    path("<int:id>/send-or-cancel/", send_or_cancel_request, name="send_or_cancel_request"),
+    path("<int:id>/accept-or-reject/", accept_or_delete_request, name="accept_or_delete_request"),
 ]
