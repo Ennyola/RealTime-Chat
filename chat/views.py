8481dbf042816761695f4505cd90946b86e74d32
Ennyola <medunoyeeni@gmail.com>
2023-02-26 20:56:00 +0100

    message now grouped by date in template

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,5 @@
+from itertools import groupby
+
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth import get_user_model
@@ -19,20 +21,28 @@ class RoomListMixin:
         rooms = self.get_rooms(user=user)
         context = {"rooms": rooms}
         return context
+
+
 class ChatIndexView(RoomListMixin, View):
     def get(self, request, *args, **kwargs):
         context = super().get_context_data(user=request.user)
         return render(request, "chat/index.html", context)
+
+
 class ChatRoomView(RoomListMixin, View):
     def get(self, request, **kwargs):
         context = super().get_context_data(user=request.user)
         messages = Message.objects.filter(room_id=kwargs["room_id"])
+        # Group messages by date so it'll be rendered accordingly on the webpage
+        grouped_messages = groupby(messages, lambda message: message.time.date())
+        message_groups = [{"date": date.strftime("%A,%B %d"), "messages": list(messages)} for date, messages in grouped_messages]
         room = Room.objects.get(id=kwargs["room_id"])
-        context["messages"] = messages
+        
         if room.room_type == "private":
             context["room_name"] = get_room_name(room.name, request.user.username)
         else:
             context["room_name"] = room.name
         friend = get_user_model().objects.get(username=context["room_name"])
+        context["message_groups"] = message_groups
         context["display_picture"] = friend.userprofile.get_image
         return render(request, "chat/chat-room.html", context)
