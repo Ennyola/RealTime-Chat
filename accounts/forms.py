8301fc361b91e51ffd3643461460545390c0c0e6
Ennyola <medunoyeeni@gmail.com>
2023-03-10 11:55:44 +0100

    styled the login and registration page

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,9 @@ from .models import UserProfile
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(label=_("Username"), max_length=100, widget=forms.TextInput(attrs={"placeholder": "Username"}))
-    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
-    confirm_password = forms.CharField(label=_("Confirm Password"), widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}))
+    username = forms.CharField(label=_("Username"), max_length=100, widget=forms.TextInput(attrs={"placeholder": "Username","class":"form-control"}))
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={"placeholder": "Password","class":"form-control"}))
+    confirm_password = forms.CharField(label=_("Confirm Password"), widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password","class":"form-control"}))
 
 class LoginForm(forms.Form):
     username = forms.CharField(label=_("Username"), max_length=100, widget=forms.TextInput(attrs={"placeholder": "Username","class":"form-control"}))
--- chat/models.py
+++ chat/models.py
@@ -73,14 +73,3 @@ class Message(models.Model):
 
     def __str__(self) -> str:
         return f"{self.content}"
-
-
-# class Chat(models.Model):
-#     participants = models.ManyToManyField(User, related_name="chats")
-#     group_name = models.CharField(max_length=100, blank=True, null=True)
-
-# class Message(models.Model):
-#     chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages")
-#     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-#     content = models.TextField()
-#     timestamp = models.DateTimeField(auto_now_add=True)
