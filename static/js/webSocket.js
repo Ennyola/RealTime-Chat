e416347f6a50909019f3cd537ce632e8ad311fbc
Ennyola <medunoyeeni@gmail.com>
2023-03-21 19:05:39 +0100

    added reconnecting websocket

--- static/js/webSocket.js
+++ static/js/webSocket.js
@@ -3,12 +3,12 @@ if (window.location.protocol == "https:") {
     endpoint = `wss://${window.location.host}/ws`;
 }
 
-export const callWebSocket = new WebSocket(`${endpoint}/call/`);
+export const callWebSocket = new ReconnectingWebSocket(`${endpoint}/call/`);
 
 export const getChatSocket = (friendName) => {
-    return new WebSocket(`${endpoint}/chat/${friendName}/`);
+    return new ReconnectingWebSocket(`${endpoint}/chat/${friendName}/`);
 }
 
-// export const friendRequestWebSocket = new WebSocket(`${endpoint}/friend-request/`);
+// export const friendRequestWebSocket = new ReconnectingWebSocket(`${endpoint}/friend-request/`);
 
-export const notificationSocket = new WebSocket(`${endpoint}/notification/`)
+export const notificationSocket = new ReconnectingWebSocket(`${endpoint}/notification/`)
