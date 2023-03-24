cb952beb442ede381518e878033485868a9a341b
Ennyola <medunoyeeni@gmail.com>
2023-03-24 12:13:50 +0100

    notification dot now disappears after 3 seconds on add friends page

--- chat/signals.py
+++ chat/signals.py
@@ -1,4 +1,3 @@
-import json
 
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
--- static/js/headerSettings.js
+++ static/js/headerSettings.js
@@ -16,7 +16,15 @@ notificationSocket.addEventListener('message', (e) => {
     let msg = JSON.parse(e.data)
     switch (msg.type) {
         case 'friend_request':
-            notificationDot.classList.remove('d-none');
+            if (notificationDot.classList.contains('d-none')) {
+                notificationDot.classList.remove('d-none');
+            }
+            // Remove the notification dot after 3 seconds if the user is on the add friend page.
+            if (window.location.pathname === "/add-friend/") {
+                setTimeout(() => {
+                    notificationDot.classList.add('d-none');
+                }, 3000)
+            }
             break;
         default:
             break;
